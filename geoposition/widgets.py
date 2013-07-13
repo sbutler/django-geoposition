cc4a49cc99e05799340f66e9a9cb70e23230d0e1
Stephen J. Butler <stephen.butler@gmail.com>
2013-07-12 21:56:11 -0500

    Get widgets from instantiator, and move the source of googleapis into the widget class

--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -1,15 +1,9 @@
+import logging
 from django import forms
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
 class GeopositionWidget(forms.MultiWidget):
-    def __init__(self, attrs=None):
-        widgets = (
-            forms.TextInput(),
-            forms.TextInput(),
-        )
-        super(GeopositionWidget, self).__init__(widgets, attrs)
-    
     def decompress(self, value):
         if value:
             return [value.latitude, value.longitude]
@@ -19,16 +13,19 @@ class GeopositionWidget(forms.MultiWidget):
         return render_to_string('geoposition/widgets/geoposition.html', {
             'latitude': {
                 'html': rendered_widgets[0],
-                'label': _("latitude"),
+                'label': _("Latitude"),
             },
             'longitude': {
                 'html': rendered_widgets[1],
-                'label': _("longitude"),
+                'label': _("Longitude"),
             },
         })
     
     class Media:
-        js = ('geoposition/geoposition.js',)
+        js = (
+                'geoposition/geoposition.js',
+                'https://maps.googleapis.com/maps/api/js?sensor=false',
+        )
         css = {
             'all': ('geoposition/geoposition.css',)
         }
